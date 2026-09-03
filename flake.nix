540d09b2e10ebf8e92e3f9cb891d736b7a72de1b
mst-mkt <mail@keito.dev>
2026-09-03 17:20:23 +0900

    add: omniwm package

--- flake.nix
+++ flake.nix
@@ -5,5 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = _: { };
+  outputs =
+    inputs:
+    let
+      omniwm = inputs.nixpkgs.legacyPackages.aarch64-darwin.callPackage ./nix/package.nix { };
+    in
+    {
+      packages.aarch64-darwin = {
+        inherit omniwm;
+        default = omniwm;
+      };
+
+      overlays.default = final: _: {
+        omniwm = final.callPackage ./nix/package.nix { };
+      };
+    };
 }
